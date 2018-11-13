541de3103d3a19f31d94bc49e1a5248ebe428973
itai4242 <itai4230@gmail.com>
2018-11-13 12:08:50 +0200

    Fixing data in roots

--- src/test/setupTests.js
+++ src/test/setupTests.js
@@ -1,8 +1,8 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
-import dotEnv from 'dotenv';
+import DotEnv from 'dotenv';
 
-dotEnv.config({ path: '.env.test'});
+DotEnv.config({ path: '.env.test'});
 
 Enzyme.configure({
     adapter: new Adapter() 
