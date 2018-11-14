c1bd326a840548888aae6165273ce5a549d81294
itai4242 <itai4230@gmail.com>
2018-11-14 15:21:03 +0200

    Add public routes

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -4,11 +4,10 @@ import createHistory from 'history/createBrowserHistory'
 import ExpenseDashboardPage from './../components/ExpenseDashboardPage';
 import ADDExpensePage from './../components/ADDExpensePage';
 import EditEepansePage from './../components/EditEepansePage';
-import HelpPage from './../components/HelpPage';
 import NotFoundPage from './../components/NotFoundPage';
-
 import LoginPage from '../components/LoginPage'
 import PrivateRoute from './PrivatRoute';
+import PublicRoute from './PublicRoute'
 
 export const history = createHistory();
 
@@ -16,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
                 <PrivateRoute path="/dashboard" component ={ExpenseDashboardPage} />
                 <PrivateRoute path="/create" component ={ADDExpensePage} />
                 <PrivateRoute path="/edit/:id" component ={EditEepansePage} />
-                <Route path="/help" component ={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div> 
